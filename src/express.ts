cf71d18f589b64dfd13f7160ef4987c65db0504f
Runar <runska@gmail.com>
2023-06-26 17:04:29 +0200

    Updating flow of express middleware, added temp logging

--- src/express.ts
+++ src/express.ts
@@ -1,14 +1,41 @@
 import { RequestHandler } from 'express';
-import { exchangeIdportenSubjectToken } from './idporten.js';
-import { setAuthorizationToken } from './header-utils.js';
+import {
+    extractSubjectToken,
+    getAuthorizationHeader,
+    setAuthorizationToken,
+} from './header-utils.js';
+import {
+    grantTokenXOboToken,
+    isInvalidTokenSet,
+    validateIdportenToken,
+} from '@navikt/next-auth-wonderwall';
+import { Logger } from './logger.js';
 
-export function idportenTokenXMiddleware(audience: string): RequestHandler {
+export function idportenTokenXMiddleware(
+    audience: string,
+    logger: Logger = console
+): RequestHandler {
     return async (req, _res, next) => {
-        const accessToken = await exchangeIdportenSubjectToken(req, audience);
-
-        if (accessToken) {
-            setAuthorizationToken(req, accessToken);
+        const authHeader = getAuthorizationHeader(req);
+        if (!authHeader) {
+            logger.info('Authorization header not found, returning');
+            return;
         }
+
+        const result = await validateIdportenToken(authHeader);
+        if (result != 'valid') {
+            logger.info('Invalid token found in authorization header');
+            return;
+        }
+
+        const grantResult = await grantTokenXOboToken(extractSubjectToken(authHeader), audience);
+        if (isInvalidTokenSet(grantResult)) {
+            logger.error(JSON.stringify(grantResult));
+            return;
+        }
+
+        logger.info('token exchange successful, updating authorization header with access token');
+        setAuthorizationToken(req, grantResult);
         next();
     };
 }
